cb50ffab4863a4dca891fd7cdffa4eab706c4757
Jef Braem <>
2018-08-07 22:17:09 +0200

    feat: Adding a Beer to the list works.

--- Beer/ViewControllers/AddViewController.swift
+++ Beer/ViewControllers/AddViewController.swift
@@ -7,28 +7,34 @@
 //
 
 import UIKit
+import RealmSwift
 
 class AddViewController: UIViewController {
+    
+    let realm = try! Realm()
+    
+    @IBOutlet weak var beerNameTextField: UITextField!
+    @IBOutlet weak var beerBreweryTextField: UITextField!
+    
+    @IBAction func saveButtonAction(_ sender: Any) {
+        
+        let beer = Beer()
+        beer.Name = beerNameTextField.text!
+        beer.Brewery = beerBreweryTextField.text!
+        
+        try! realm.write {
+            realm.add(beer)
+        }
+        
+        navigationController?.popViewController(animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Beer/ViewControllers/ListViewController.swift
+++ Beer/ViewControllers/ListViewController.swift
@@ -9,38 +9,53 @@
 import UIKit
 import RealmSwift
 
+var beers: Results<Beer>!
+var realm = try! Realm()
+
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var beersTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        beers = realm.objects(Beer.self)
         beersTableView.dataSource = self
         beersTableView.delegate = self
+        reload()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        reload()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "BeerCell", for: indexPath) as! BeerCell
-        cell.LeftCell.text = "Hoegaarden"
-        cell.RightCell.text = "4/5"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BeerCell", for: indexPath) as! BeerCell
+        let todo = beers[indexPath.row]
+        cell.LeftCell.text = todo.Name
+        cell.RightCell.text = todo.Brewery
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return beers.count
+    }
+    
+    func reload() {
+        beersTableView.reloadData()
     }
 }
 
-class ToDo: Object {
-    dynamic var Name = ""
-    dynamic var Score = 0
+class Beer: Object {
+    @objc dynamic var Name = ""
+    @objc dynamic var Brewery = ""
 }
 
 class BeerCell: UITableViewCell {
     @IBOutlet weak var LeftCell: UILabel!
     @IBOutlet weak var RightCell: UILabel!
 }
+
+
